07323b4f09bc6cf6575b85bf747b7e71c5ee63ab
Rafly <raflyzzz424@gmail.com>
2026-04-16 14:56:33 +0700

    fix: add root endpoint

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,19 @@ app.use(express.json());
 // Routes
 import authRoutes from './routes/auth.routes';
 import kosRoutes from './routes/kos.routes';
-import fasilityRoutes from './routes/facility.routes';
+import facilityRoutes from './routes/facility.routes';
 import reviewRoutes from './routes/review.routes';
 import bookingRoutes from './routes/booking.routes';
 import userRoutes from './routes/user.routes';
 app.use('/auth', authRoutes);
 app.use('/kos', kosRoutes);
-app.use('/kos/:kosId/fasilities', fasilityRoutes);
+app.use('/kos/:kosId/facilities', facilityRoutes);
 app.use('/kos/:kosId/reviews', reviewRoutes);
 app.use('/bookings', bookingRoutes);
-app.use('/users', userRoutes);
+app.use('/users', userRoutes);app.get('/', (req, res) => {
+  res.send('API Kos Hunter jalan 🚀');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
